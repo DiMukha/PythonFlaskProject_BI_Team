b3673296227b2dc8ba519b4f22ffad98e9ad2d4e
DiMukha <lutique2012@gmail.com>
2020-12-29 15:41:01 +0200

    create app_table_data_view, check links

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, redirect
 from flask_bootstrap import Bootstrap
 
-from dataset_view_app.import_db_data import load_sales_data
 from settings.config import SECRET_KEY
 
 import auth
 import db
 import app_export
-
+from dataset_view_app import app_table_data_view
 
 app = Flask(__name__)
 Bootstrap(app)
 db.init_app(app)
 app.register_blueprint(auth.bp)
 app.register_blueprint(app_export.bp)
+app.register_blueprint(app_table_data_view.bp)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 
@@ -65,27 +65,6 @@ def update_user(id_):
     db.close()
     return render_template('update_user.html', **user_data)
 
-
-@app.route('/data_view/')
-@app.route('/data_view/<int:page>')
-def data_view(page=0):
-    columns, data = load_sales_data()
-    prev = page
-    page_from = page
-    next = prev + 1
-    if page > 0:
-        prev = page - 1
-        next = prev + 2
-        page_from = page * 10
-    page_to = page_from + 10
-    data = data[page_from:page_to]
-    return render_template('data_view/table_data.html',
-                           columns=columns,
-                           data=data,
-                           prev=prev,
-                           next=next,
-                           page_from=page_from,
-                           page_to=page_to)
 
 if __name__ == '__main__':
     app.run(debug=True)
