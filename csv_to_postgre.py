38c31084cb2f06b74efe78312ff045020cbbfb15
DiMukha <lutique2012@gmail.com>
2020-12-29 15:03:39 +0200

    check csv_to_postgre.py

--- csv_to_postgre.py
+++ csv_to_postgre.py
@@ -1,5 +1,7 @@
 import psycopg2
 
+from settings.config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
+
 
 def csv_to_postgre():
     db = None
@@ -7,11 +9,10 @@ def csv_to_postgre():
     ct_script = 'sales_data.sql'
     try:
         # connect to the PostgreSQL db
-        db = psycopg2.connect(dbname='db_flask_bi',
-                              user='bi_user',
-                              password='postgreSQL20',
-                              host='167.71.78.60',
-                              port=5432)
+        db = psycopg2.connect(dbname=DB_NAME,
+                              user=DB_USER,
+                              password=DB_PASSWORD,
+                              host=DB_HOST)
         cur = db.cursor()
         cur.execute("DROP TABLE IF EXISTS sales_data;")
         # create table from script
