ed675c36f5db52c1809a96a4d91d345c04af632d
DiMukha <lutique2012@gmail.com>
2021-01-13 23:45:53 +0200

    bug fix filter dataset and pagination

--- dataset_view_app/app_table_data_view.py
+++ dataset_view_app/app_table_data_view.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, send_file, request, session
+from flask import render_template, Blueprint, send_file, request, session, url_for
 from xlsxwriter import Workbook
 
 import auth
@@ -8,20 +8,27 @@ bp = Blueprint('table_data', __name__, url_prefix='/table_data')
 
 
 @bp.route('/data_view', methods=['GET', 'POST'])
-@bp.route('/data_view/<int:page>', methods=['GET', 'POST'])
 @auth.login_required
-def data_view(page=0):
+def data_view():
     filters = {parameter: value for parameter, value in request.args.items() if value}
     default_statuses = load_statuses()
-    offset_page = page
+    offset_page = int(filters.get('page', 0))
     columns, data = load_sales_data(filters, offset_page)
-    prev = page
-    page_from = page
-    next = prev + 1
-    if page:
-        prev = page - 1
-        next = prev + 2
-        page_from = page * 10
+
+    base_url = f"{ url_for('table_data.data_view') }" \
+           f"?order_num={filters.get('order_num', '')}" \
+           f"&order_status={filters.get('order_status', '')}" \
+           f"&min_order_date={filters.get('min_order_date', '')}" \
+           f"&max_order_date={filters.get('max_order_date', '')}" \
+           f"&min_price={filters.get('min_price', '')}" \
+           f"&max_price={filters.get('max_price', '')}"
+
+    prev = base_url + f"&page={offset_page}"
+    if offset_page:
+        prev = base_url + f"&page={offset_page-1}"
+    next = base_url + f"&page={offset_page+1}"
+
+    page_from = offset_page*10
     page_to = page_from + 10
     return render_template('data_view/table_data.html',
                            columns=columns,
