dfc3ec332bfd14120e51faf92bd115eb8d51fc8e
DiMukha <lutique2012@gmail.com>
2021-01-19 14:22:02 +0200

    add update user form

--- app/users/users_list.py
+++ app/users/users_list.py
@@ -1,8 +1,9 @@
-from flask import render_template, redirect, flash, url_for
+from flask import render_template, redirect, flash, url_for, request
 from flask_login import login_required
 
 from app import app, db
 from app.models import User
+from .forms import UpdateForm
 
 
 @app.route('/list_users')
@@ -22,33 +23,19 @@ def delete_user(login):
     return redirect(url_for('index'))
 
 
-# @app.route('/update_user/<int:id_>', methods=['GET', 'POST'])
-# def update_user(id_):
-#     attributes = ['firstname', 'lastname', 'login', 'email']
-#     user_data = {k: '' for k in attributes}
-#     db = sqlite3.connect('db.report_system')
-#     cursor = db.cursor()
-#     if request.method == 'GET':
-#         cursor.execute('select firstname, lastname, login, email from users where id = ?', (id_,))
-#         data = cursor.fetchone()
-#         for i in range(len(attributes)):
-#             user_data[attributes[i]] = data[i]
-#     if request.method == 'POST':
-#         firstname = request.form.get('firstname', '')
-#         lastname = request.form.get('lastname', '')
-#         login = request.form.get('login', '')
-#         email = request.form.get('email', '')
-#         if firstname and lastname and login and email:
-#             cursor.execute('''update users
-#                               set firstname = ?, lastname = ?, login = ?, email = ?
-#                               where id = ? ''', (firstname, lastname, login, email, id_))
-#         cursor.close()
-#         db.commit()
-#         db.close()
-#         print('yo')
-#         return redirect(url_for('users.list_users'))
-#     cursor.close()
-#     db.commit()
-#     db.close()
-#
-#     return render_template('users/update_user.html', **user_data)
+@app.route('/update_user/<login>', methods=['GET', 'POST'])
+@login_required
+def update_user(login):
+    user = User.query.filter_by(login=login).first()
+    form = UpdateForm()
+    print(form.validate_on_submit())
+    if request.method == 'POST' and form.validate_on_submit():
+        print(request.method)
+        user.first_name = form.first_name.data
+        user.last_name = form.last_name.data
+        user.email = form.last_name.data
+        db.session.commit()
+        flash(f'User {user.login} is updated!')
+        return redirect(url_for('list_users'))
+    return render_template('users/update_user.html', form=form, user=user)
+
