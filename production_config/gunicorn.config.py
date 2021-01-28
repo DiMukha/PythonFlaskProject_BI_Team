8672dface1397277f1d242ec7acbc2779c29a8d9
DiMukha <lutique2012@gmail.com>
2021-01-28 12:34:33 +0200

    create production

---
+++ production_config/gunicorn.config.py
@@ -0,0 +1,4 @@
+bind = '127.0.0.1:5000'
+workers = 2
+user = "lutique"
+timeout = 120
--- report_system.py
+++ report_system.py
@@ -2,5 +2,4 @@ from app import app
 
 
 if __name__ == '__main__':
-
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host="167.71.78.60")
